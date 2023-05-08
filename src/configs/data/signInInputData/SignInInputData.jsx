dce740bb4de50fdddcc975e0157949519606bb66
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-05-08 11:56:23 -0400

    Nothing changed.

--- src/configs/data/signInInputData/SignInInputData.jsx
+++ src/configs/data/signInInputData/SignInInputData.jsx
@@ -16,7 +16,7 @@ const SignInInputData = [
     },
   },
   {
-    frameClass: "mb-12 w-full",
+    frameClass: "mb-6 w-full",
     inputClass:
       "linear-tight focus:shadow-outline w-full appearance-none rounded border border-customGreen py-1 px-1 text-gray-700 focus:outline-none",
     labelClass: "mb-2 text-xs font-bold uppercase tracking-wide text-gray-700",
