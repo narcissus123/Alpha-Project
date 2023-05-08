d0b57fb4093372bab279fdfab056124ed74d8ad4
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-05-08 11:34:46 -0400

    Changed variable name.

--- src/core/services/api/ManageStudent.api.jsx
+++ src/core/services/api/ManageStudent.api.jsx
@@ -3,6 +3,7 @@ import { instance } from "../interceptor/Interceptor";
 const updateStudentInfo = async (userId, obj) => {
   try {
     const response = await instance.put(`/api/student/${userId}`, obj);
+    console.log(response);
     return response;
   } catch (error) {
     return error;
