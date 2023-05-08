24e577067419c74e93f61f020f9dd29ca5880f25
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-05-08 12:02:26 -0400

    Changed style.

--- src/components/userPanelContainer/UserPanelContainer.jsx
+++ src/components/userPanelContainer/UserPanelContainer.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { EnrolledCourses } from "./enrolledCourses/EnrolledCourses";
 import { AllCourses } from "./allCourses/AllCourses";
 import { EditProfile } from "./editProfile/EditProfile";
+import { UploadImageModal } from "./uploadImageModal/UploadImageModal";
 
 import { getItem } from "../../core/services/storage/Storage";
 
@@ -27,6 +28,7 @@ const UserPanelContainer = (props) => {
 
   const [show, setShow] = useState(false);
 
+  const [openImgModal, setOpenImgModal] = useState(false);
   return (
     <div class="flex h-[43rem]">
       {/* User panel sidebar */}
@@ -39,16 +41,34 @@ const UserPanelContainer = (props) => {
       >
         <div class="h-full w-full">
           <div class="ml-1/12 mt-12 flex w-10/12 items-center gap-4 rounded-xl border bg-white p-2 text-slate-800 lg:mx-auto lg:mt-14 lg:w-11/12 lg:p-3">
-            <div class="rounded-full border border-customGreen ">
-              <UserImage3 />
+            <div class="h-14 w-14 overflow-hidden rounded-full border border-customGreen ">
+              {user.profile ? (
+                <img src={user.profile} class="h-14 w-14 object-cover " />
+              ) : (
+                <UserImage3 />
+              )}
             </div>
             <span class="sm:text-sm md:text-base lg:text-lg">
               {user.fullName}
             </span>
-            <div class="ml-2 hidden h-14 border-l border-customGreen pt-[0.9rem] pl-2 text-center lg:block">
+            <div class="ml-2 hidden h-14 justify-center gap-3 border-l border-customGreen pl-2 text-center lg:flex lg:flex-col">
               <Link to="/logout">
-                <LogoutImage Class="pointer-cursor h-6 w-6 " />
+                <LogoutImage Class="pointer-cursor h-5 w-5 " />
               </Link>
+              <button onClick={() => setOpenImgModal(true)}>
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 640 512"
+                  height="1.3rem"
+                  width="1.3rem"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="bg-white hover:text-[#747bff]"
+                >
+                  <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h274.9c-2.4-6.8-3.4-14-2.6-21.3l6.8-60.9 1.2-11.1 7.9-7.9 77.3-77.3c-24.5-27.7-60-45.5-99.9-45.5zm45.3 145.3l-6.8 61c-1.1 10.2 7.5 18.8 17.6 17.6l60.9-6.8 137.9-137.9-71.7-71.7-137.9 137.8zM633 268.9L595.1 231c-9.3-9.3-24.5-9.3-33.8 0l-37.8 37.8-4.1 4.1 71.8 71.7 41.8-41.8c9.3-9.4 9.3-24.5 0-33.9z"></path>
+                </svg>
+              </button>
             </div>
           </div>
 
@@ -112,7 +132,9 @@ const UserPanelContainer = (props) => {
         </div>
       </div>
       {/* User panel content area */}
+
       <div class="w-full bg-white lg:w-8/10">
+        {openImgModal && <UploadImageModal setOpenImgModal={setOpenImgModal} />}
         {active === 1 && <EditProfile programInfo={props.programInfo} />}
         {active === 2 && <EnrolledCourses programInfo={props.programInfo} />}
         {active === 3 && <AllCourses programInfo={props.programInfo} />}
