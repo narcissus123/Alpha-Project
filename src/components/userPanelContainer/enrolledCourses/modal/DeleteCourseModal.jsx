5e8f01cbe78719b1bfd52fa2768bbe95271fe0a6
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-05-08 11:59:43 -0400

    Used custom modal component.

--- src/components/userPanelContainer/enrolledCourses/modal/DeleteCourseModal.jsx
+++ src/components/userPanelContainer/enrolledCourses/modal/DeleteCourseModal.jsx
@@ -6,6 +6,8 @@ import { Modal } from "../../../common/modal/Modal";
 import { deleteCourse } from "../../../../core/services/api/Courses.api";
 import { getItem } from "../../../../core/services/storage/Storage";
 
+import { NotifyImage } from "../../../../assets/svg/Svg";
+
 // This component shows the modal. Users should verify that they want to drop the course they selected.
 const DeleteCourseModal = ({ setOpen, selectedCourse }) => {
   const [isSubmitting, seIsSubmitting] = useState(false);
@@ -16,7 +18,7 @@ const DeleteCourseModal = ({ setOpen, selectedCourse }) => {
 
       seIsSubmitting(true);
       const response = await deleteCourse(selectedCourse._id, userInfo._id);
-      if (response.success) {
+      if (response.status === 200) {
         toast.success("Course successfully has been deleted.");
         seIsSubmitting(false);
       }
@@ -35,11 +37,33 @@ const DeleteCourseModal = ({ setOpen, selectedCourse }) => {
         setOpen={setOpen}
         buttonText="Delete"
         buttonClass="hover:bg-red-500 bg-red-600"
-        title="Delete Course"
-        message="Are you sure you want to delete this course?"
         handleCourseRegistration={handleCourseRegistration}
         isSubmitting={isSubmitting}
-      />
+      >
+        {/* Content */}
+        <div class="shadow-xl relative transform  overflow-hidden rounded-b-lg border bg-white text-left transition-all sm:w-full sm:max-w-lg">
+          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <div class="bg-white-100 mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full border border-yellow-600 sm:mx-0 sm:h-10 sm:w-10">
+                <NotifyImage />
+              </div>
+              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  class="text-base font-semibold leading-6 text-gray-900"
+                  id="modal-title"
+                >
+                  "Delete Course"
+                </h3>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    "Are you sure you want to delete this course?"
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </Fragment>
   );
 };
