f75c7917501c6d9a47c4c93f699c7ad4e89ca329
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-05-08 11:52:19 -0400

    Made changes to dropdown menue.

--- src/components/homeContainer/landingSection/LandingSection.jsx
+++ src/components/homeContainer/landingSection/LandingSection.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useFetch } from "../../../hooks/useFetch";
+import { getCourses } from "../../../core/services/api/Courses.api";
 
 // This component renders the name of institute and provides a brief description of the institution.
 const LandingSection = () => {
+  const history = useNavigate();
+
+  /* Call API to get courses.*/
+  const { isLoading, data } = useFetch(getCourses);
+
+  const items = data.map((course) => ({
+    id: course._id,
+    name: course.lesson.lessonName,
+    link: `/programs/${course._id}`,
+    image: course.lesson.image,
+  }));
+
+  // The item selected
+  const handleOnSelect = (item) => {
+    history(item.link);
+  };
+
+  const formatResult = (item) => {
+    return (
+      <>
+        <li class="flex cursor-pointer flex-row items-center gap-3 text-left hover:text-[#747bff]">
+          <img src={item.image} class="w-10" />
+          <span>Course: {item.name}</span>
+        </li>
+      </>
+    );
+  };
+
   return (
-    <div class="h-[43rem] overflow-hidden bg-landingIntroImg bg-cover bg-no-repeat opacity-70">
+    <div class="h-[43rem] bg-landingIntroImg bg-cover bg-no-repeat opacity-70">
       <div class=" mx-auto mt-40 space-y-16 text-center text-lg text-white">
         <h1 class="text-4xl font-semibold text-customGreen sm:text-5xl lg:text-6xl">
           Alpha Institute
@@ -14,6 +47,15 @@ const LandingSection = () => {
             We are proud to serve you with the strongest training staff in
             person and online.
           </p>
+        </div>
+        <div class="z-30 mx-auto w-72 lg:w-80">
+          <ReactSearchAutocomplete
+            placeholder="Search course name here"
+            items={items}
+            onSelect={handleOnSelect}
+            autoFocus
+            formatResult={formatResult}
+          />
         </div>
       </div>
     </div>
