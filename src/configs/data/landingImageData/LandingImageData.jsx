0f1c91603abe72dc22756d08c5639bdf79936342
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 10:28:40 -0400

    image for landing page.

---
+++ src/configs/data/landingImageData/LandingImageData.jsx
@@ -0,0 +1,22 @@
+import landing1 from "../../../assets/img/landing1.png";
+import landing2 from "../../../assets/img/landing2.jpg";
+import landing3 from "../../../assets/img/landing3.jpg";
+import landing4 from "../../../assets/img/landing4.webp";
+import landing5 from "../../../assets/img/landing5.jpg";
+export const sliderData = [
+  {
+    src: landing1,
+  },
+  {
+    src: landing2,
+  },
+  {
+    src: landing3,
+  },
+  {
+    src: landing4,
+  },
+  {
+    src: landing5,
+  },
+];
