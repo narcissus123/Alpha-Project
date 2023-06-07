b7e471df2f05c4e8c44ba788be772e749c5baa6e
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 11:10:14 -0400

    Added ChatWithUs component.

--- src/components/homeContainer/HomeContainer.jsx
+++ src/components/homeContainer/HomeContainer.jsx
@@ -2,15 +2,18 @@ import { LandingSection } from "./landingSection/LandingSection";
 import { EnrollmentSection } from "./enrollmentSection/EnrollmentSection";
 import { CoursesIntroSection } from "./coursesIntroSection/CoursesIntroSection";
 import { NewsSection } from "./newsSection/NewsSection";
-
+import { ChatWithUs } from "./chatWithUs/ChatWithUs";
 // This component renders different sections of a home page.
 const HomeContainer = () => {
   return (
-    <div class="grid-col z-0 grid overflow-hidden">
+    <div class="grid-col relative z-0 grid overflow-hidden">
       <LandingSection />
       <EnrollmentSection />
       <CoursesIntroSection />
       <NewsSection />
+      <div class="fixed top-0 left-0 right-0 z-50">
+        <ChatWithUs />
+      </div>
     </div>
   );
 };
