828a5a2f174599c3529bff5f942ddbcb331bdea7
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 10:40:11 -0400

    Added logout component for admin to logout from alpha institute website and returns back to dashboard.

---
+++ src/components/adminLogoutContainer/AdminLogoutContainer.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+import { SignOutAdmin } from "../../core/services/api/admin-authentication.api";
+import { useAuth } from "../../context/AuthContext";
+import { getItem } from "../../core/services/storage/Storage";
+
+// Users will lead to this component when they click on the logout button.
+// The local storage will be cleared and the public routes will be rendered instead of private routes.
+const AdminLogoutContainer = () => {
+  const user = useAuth();
+
+  console.log(user.isAdmin);
+  useEffect(() => {
+    if (Boolean(getItem("admin")) === true) {
+      SignOutAdmin();
+
+      user.loginAsAdmin(Boolean(getItem("admin")) === true);
+
+      window.location.href = "http://localhost:5173/";
+    }
+  });
+
+  return null;
+};
+
+export { AdminLogoutContainer };
