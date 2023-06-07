33c7aa5f0c806802c2af34fda3eeb2715c9b5d98
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-07 10:23:01 -0400

    Component for admin to logout.

---
+++ src/screens/adminLogout/AdminLogout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { AdminLogoutContainer } from "../../components/adminLogoutContainer/AdminLogoutContainer";
+
+const AdminLogout = () => {
+  return <AdminLogoutContainer />;
+};
+
+export { AdminLogout };
