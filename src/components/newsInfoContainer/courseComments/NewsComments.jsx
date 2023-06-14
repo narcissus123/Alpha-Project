379b5848bbb0b480516aaef2718e7fe02903f1fc
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-14 15:26:51 -0400

    Added reply and verify option to NewsComments component for admin.

--- src/components/newsInfoContainer/courseComments/NewsComments.jsx
+++ src/components/newsInfoContainer/courseComments/NewsComments.jsx
@@ -1,64 +1,67 @@
 import { useState, useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 import { AlertMessage } from "../../common/messages/alertMessage/AlertMessage";
 import { CommentCard } from "./commentCard/CommentCard";
 
-import { FilterValidComments } from "../../../core/utils/Filter";
+import {
+  FilterValidComments,
+  FilterUnrelatedComments,
+} from "../../../core/utils/Filter";
 import { getAllComments } from "../../../core/services/api/comments.api";
+import { useFetch } from "../../../hooks/useFetch";
+import { useAuth } from "../../../context/AuthContext";
 
 // This component renders users comments about the news in the news page and displays the admin responses to users comments.
 const NewsComments = ({ param }) => {
+  const admin = useAuth();
+
+  /* Getting  user comments*/
+  const { data } = useFetch(getAllComments);
+
   /* Saving  user comments*/
-  const [comment, setComments] = useState([]);
+  const [comments, setComments] = useState([]);
 
   /* Sending request to get news comments */
-  const newsComments = async () => {
-    try {
-      const response = await getAllComments();
-      const filteredComments = FilterValidComments(response, param.newsId); //Filtering unverified comments
-
-      setComments(filteredComments);
-    } catch (error) {
-      toast.error(error);
-    }
-  };
-
   useEffect(() => {
-    newsComments();
-  }, []);
+    const filteredComments = admin.isAdmin
+      ? FilterUnrelatedComments(data, param.newsId)
+      : FilterValidComments(data, param.newsId); //Filtering unverified comments
+    setComments(filteredComments);
+  }, [data]);
 
   return (
     <section class="opacity-3 w-screen bg-white py-8 lg:py-16">
       <ToastContainer />
-      {comment != [] ? (
+      {comments != [] ? (
         <div class="ml-8 flex max-w-6xl flex-col border-t border-customGreen2 px-4 text-slate-800">
           <>
             {/* Title */}
             <div class="mb-6">
               <h2 class="py-4 text-lg font-bold text-customGreen2 dark:text-white lg:text-2xl">
-                Discussion ({comment.length})
+                Discussion ({comments.length})
               </h2>
             </div>
             {/* This section displays verified commnets and admit response to those comments if there is any. */}
-            {comment.map((comm) => (
+            {comments.map((comm, index) => (
               <>
-                <CommentCard Class={"w-2/3"} comment={comm} answer={false} />
-                {comm.answer ? (
+                <CommentCard
+                  Class={"w-2/3"}
+                  comment={comm}
+                  answer={false}
+                  key={comm._id}
+                  setComments={setComments}
+                  allComments={comments}
+                />
+                {comm.answer && (
                   <CommentCard
+                    key={index}
                     Class={"ml-[6.5%] w-3/5 "}
                     comment={comm}
                     answer={true}
+                    setComments={setComments}
+                    allComments={comments}
                   />
-                ) : (
-                  comm.answer === undefined && (
-                    <AlertMessage
-                      message="No replies."
-                      Class={
-                        "py-2 text-base border ml-[6.5%] w-3/5 rounded-lg mb-8 text-gray-600 text-left"
-                      }
-                    />
-                  )
                 )}
               </>
             ))}
