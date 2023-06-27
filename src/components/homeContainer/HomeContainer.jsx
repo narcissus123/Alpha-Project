bb42a53b0128c7bc7cf8a63f12e8d44939cde572
narcissus123 <39791410+narcissus123@users.noreply.github.com>
2023-06-27 10:15:25 -0400

    Changed name of variable from auth to user.

--- src/components/homeContainer/HomeContainer.jsx
+++ src/components/homeContainer/HomeContainer.jsx
@@ -3,15 +3,24 @@ import { EnrollmentSection } from "./enrollmentSection/EnrollmentSection";
 import { CoursesIntroSection } from "./coursesIntroSection/CoursesIntroSection";
 import { NewsSection } from "./newsSection/NewsSection";
 import { ChatWithUs } from "./chatWithUs/ChatWithUs";
+
+import { useAuth } from "../../context/AuthContext";
+
 // This component renders different sections of a home page.
 const HomeContainer = () => {
+  const user = useAuth();
+
   return (
     <div class="grid-col relative z-0 grid overflow-hidden">
       <LandingSection />
       <EnrollmentSection />
       <CoursesIntroSection />
       <NewsSection />
-      <div class="fixed top-0 left-0 right-0 z-50">
+      <div
+        class={`${
+          user.isAdmin || user.isStudent ? "fixed" : "hidden"
+        }  top-0 left-0 right-0 z-50`}
+      >
         <ChatWithUs />
       </div>
     </div>
